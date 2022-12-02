907dcb8047a16eb70df15c92d446f5fa8f76de89
Rafe <rafenasir@gmail.com>
2022-12-02 19:15:46 +0100

    added query parameter

--- src/components/DialogueBox.js
+++ src/components/DialogueBox.js
@@ -7,6 +7,7 @@ import DialogContent from "@mui/material/DialogContent";
 import DialogContentText from "@mui/material/DialogContentText";
 import DialogTitle from "@mui/material/DialogTitle";
 import MenuItem from "@mui/material/MenuItem";
+import useQuery from "@tanstack/react-query";
 
 const filters = [
   { value: "Yes", label: "Want To Switch" },
